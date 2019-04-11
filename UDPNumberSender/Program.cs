6b7fabcc999c81776ec3e345c0386a6598f6844b
Marcel Kristensen <36763180+Razhork@users.noreply.github.com>
2019-04-11 12:41:36 +0200

    Add project files.

---
+++ UDPNumberSender/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace UDPNumberSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+
+            IPAddress ip = IPAddress.Parse("127.0.0.1"); //
+            UdpClient udpClient = new UdpClient("127.0.0.1", 9999);
+
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(ip, 9999); //
+            //udpClient.Connect(RemoteIpEndPoint); //
+
+            ; Console.Write("State name: ");
+            String name = Console.ReadLine();
+            while (true)
+            {
+
+                for (int i = 0; i < 5000; i++)
+                {
+                    Byte[] sendBytes = Encoding.ASCII.GetBytes(name + " " + number + " hello");
+                    udpClient.Send(sendBytes, sendBytes.Length); //, RemoteEndPoint);
+                    number++;
+                    Thread.Sleep(100);
+                }
+            }
+        }
+    }
+}
---
+++ UDPSenderBroadcast/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace UDPSenderBroadcast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+
+            IPAddress ip = IPAddress.Parse("127.0.0.1"); //
+            //UdpClient udpClient = new UdpClient("127.0.0.1", 9999);
+            UdpClient udpSender = new UdpClient( 0);
+            udpSender.EnableBroadcast = true;
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Broadcast, 9999);
+            Console.Write("State name: ");
+            String name = Console.ReadLine();
+
+            while (true)
+            {
+                for (int i = 0; i < 5000; i++)
+                {
+                    Byte[] sendBytes = Encoding.ASCII.GetBytes(name + " " + number + " hello");
+
+                    udpSender.Send(sendBytes, sendBytes.Length, RemoteIpEndPoint); //, RemoteEndPoint);
+                    //Client is now activated");
+                    number++;
+                    Thread.Sleep(100);
+                }
+            }
+        }
+    }
+}
